9f660883cd7f4ec23f72fafdc24a8b71fc7efd66
Seongwoo Kim <kimseongu15@gmail.com>
2024-01-17 15:25:14 +0000

    2024-01-18

--- bert_base.py
+++ bert_base.py
@@ -5,21 +5,24 @@ import numpy as np
 import evaluate
 from torch.optim import AdamW
 from utils import EarlyStoppingCallback
+from transformers import BertConfig
+
+
 
 def tokenize_function(examples):
-    return tokenizer(examples["sentence"], padding="max_length", max_length=128)
+    return tokenizer(examples["sentence"], padding="max_length", max_length=512)
 
 def tokenize_function_2(examples):
-    return tokenizer(examples["sentence1"], examples["sentence2"], padding="max_length", truncation=True, max_length=128)
+    return tokenizer(examples["sentence1"], examples["sentence2"], padding="max_length", truncation=True, max_length=512)
 
 def tokenize_function_3(examples):
-    return tokenizer(examples["question"], examples["sentence"], padding="max_length", truncation=True, max_length=128)
+    return tokenizer(examples["question"], examples["sentence"], padding="max_length", truncation=True, max_length=512)
 
 def tokenize_function_4(examples):
-    return tokenizer(examples["premise"], examples["hypothesis"], padding="max_length", truncation=True, max_length=128)
+    return tokenizer(examples["premise"], examples["hypothesis"], padding="max_length", truncation=True, max_length=512)
 
 def tokenize_function_5(examples):
-    return tokenizer(examples["question1"], examples["question2"], padding="max_length", truncation=True, max_length=128)
+    return tokenizer(examples["question1"], examples["question2"], padding="max_length", truncation=True, max_length=512)
 
 
 def compute_metrics(eval_pred):
@@ -28,10 +31,10 @@ def compute_metrics(eval_pred):
     return metric.compute(predictions=predictions, references=labels)
 
 dataset_list = ['wnli', 'sst2', 'rte', 'qnli', 'mrpc', 'cola', 'mnli', 'qqp']
-dataset_name = 'wnli'
+dataset_name = 'sst2'
 
 dataset = load_dataset("glue", dataset_name)
-print(dataset)
+#print(dataset)
 print(dataset['train'][0])
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
@@ -54,7 +57,12 @@ else:
     shuffle_eval_dataset = tokenized_datasets["validation"].shuffle(seed=42)
     num_labels = 2
 
-model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels = num_labels)
+# Load the configuration for BERT, set the dropout rate to 0.1, and specify the number of labels
+config = BertConfig.from_pretrained("bert-base-uncased", hidden_dropout_prob=0.1, num_labels=num_labels)
+
+# Now load the model with the updated configuration
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", config=config)
+
 
 metric = evaluate.load("accuracy")
 # Create an instance of your custom callback
@@ -63,18 +71,17 @@ early_stopping_callback = EarlyStoppingCallback(early_stopping_patience=5)
 
 training_args = TrainingArguments(
     output_dir="train_" + dataset_name, 
-    evaluation_strategy="steps",
+    evaluation_strategy="epoch",
+    save_strategy= 'epoch',
     per_device_train_batch_size=32, 
     per_device_eval_batch_size=32,
-    learning_rate=0.0001,
-    max_steps=75000,
-    num_train_epochs=10000000.0,
+    learning_rate=0.00005,
+    num_train_epochs=4.0,
     load_best_model_at_end=True,
     metric_for_best_model="accuracy",
     greater_is_better=True,
     fp16=True,
 )
-#early_stopping_patience=5,
 
 print(training_args)
 
@@ -84,7 +91,6 @@ trainer = Trainer(
     train_dataset=shuffle_train_dataset,
     eval_dataset=shuffle_eval_dataset,
     compute_metrics=compute_metrics,
-    callbacks=[early_stopping_callback],
 )
 
 trainer.train()
